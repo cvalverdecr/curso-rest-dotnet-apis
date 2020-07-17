77749caef7546f43a120b5855fa241c438d4f6cf
Carlos Valverde <cevc1184@gmail.com>
2020-07-16 20:53:23 -0600

    Laboratorio cuarta sesion

--- material/CursoDotNetRestApis/WebApp/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace WebApp
-{
-    public class Product
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-    }
-}
--- material/WebApp/Pages/Index.cshtml.cs
+++ material/WebApp/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -17,9 +20,29 @@ namespace WebApp.Pages
             _logger = logger;
         }
 
-        public void OnGet()
-        {
 
+        public async Task<IActionResult> OnGet()
+        {
+            using (var client = new HttpClient ())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000/api/" /*Web API*/ );
+
+                // Request
+                var result = await client.GetStringAsync("products");
+
+                _logger.LogInformation("Response: {0}", result);
+
+                // Deserializacion (JSON -> Object)
+
+                Product[] models = JsonSerializer.Deserialize<Product[]>(result);
+
+                ViewData["Models"] = models;
+
+                // 1) Cambiar Id=>id en Producto
+                // 2) Mostrar los productos en el HTML/Razor
+
+                return Page();
+            }
         }
     }
 }
